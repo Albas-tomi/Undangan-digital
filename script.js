7c3d5d0f4e3223e2fc3b479714e49811377fedac
dewandra <75962411+dewandra@users.noreply.github.com>
2023-06-29 09:51:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
+// ------------------------------------------- GET URL PARAM -------------------------------------------
+if (window.location.search) {
+  const valueKeys = window.location.search;
+  const value = new URLSearchParams(valueKeys);
+  valueName = value.get("name");
+  document.getElementById("nama").innerHTML = valueName;
+  document.getElementById("nama-undangan").innerHTML = valueName;
+}
+// ------------------------------------------- END GET URL PARAM -------------------------------------------
+
+// ------------------------------------------- BUKA MODAL AUTOPLAY MUSIC -------------------------------------------
 const buka = async () => {
-  document.getElementById("navigation").removeAttribute("id");
   document.getElementById("modalKu").removeAttribute("id");
   document.getElementById("tombol-musik").style.display = "block";
   audio.play();
-  const btn = document.getElementById("tombol-musik");
-  btn.innerHTML = '<i class="bi bi-pause-circle"></i>';
-  btn.style.display("block");
 };
+// ------------------------------------------- END BUKA MODAL AUTOPLAY MUSIC -------------------------------------------
 
+// ------------------------------------------- PLAY MUSIC -------------------------------------------
 const audio = (() => {
   var instance = undefined;
 
@@ -42,10 +51,50 @@ const play = (btn) => {
   if (btn.getAttribute("data-status").toString() != "true") {
     btn.setAttribute("data-status", "true");
     audio.play();
-    btn.innerHTML = '<i class="bi bi-pause-circle"></i>';
+    btn.innerHTML = '<i class="fa-solid fa-circle-pause"></i>';
   } else {
     btn.setAttribute("data-status", "false");
     audio.pause();
-    btn.innerHTML = '<i class="bi bi-play-circle"></i>';
+    btn.innerHTML = '<i class="fa-solid fa-circle-play"></i>';
   }
 };
+
+// ------------------------------------------- END PLAY MUSIC -------------------------------------------
+
+// ------------------------------------------- DATE -------------------------------------------
+let hariPernikahan = new Date(2023, 6, 7);
+console.log(hariPernikahan);
+
+let hari = document.getElementById("hari");
+let jam = document.getElementById("jam");
+let menit = document.getElementById("menit");
+let detik = document.getElementById("detik");
+
+setInterval(function () {
+  let now = new Date();
+  let waktuTunggu = (hariPernikahan - now) / 1000;
+  updateJam(waktuTunggu);
+}, 1000);
+
+const updateJam = (sisaWaktu) => {
+  let harii = Math.floor(sisaWaktu / 86400);
+  sisaWaktu -= harii * 86400;
+
+  let jams = Math.floor(sisaWaktu / 3600);
+  sisaWaktu -= jams * 3600;
+
+  let menits = Math.floor(sisaWaktu / 60);
+  sisaWaktu -= menits * 60;
+
+  let detiks = Math.floor(sisaWaktu % 60);
+
+  hari.innerHTML = Number(harii);
+  jam.innerHTML = Number(jams);
+  menit.innerHTML = Number(menits);
+  detik.innerHTML = Number(detiks);
+};
+
+const Number = (number) => {
+  return number < 10 ? "0" + number : number;
+};
+// ------------------------------------------- END DATE -------------------------------------------
